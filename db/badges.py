cad89c5c00201297bfd0b384b743967fd4c998f1
AnthonyRVa <anthonyrubiovasquez@gmail.com>
2025-10-08 10:42:46 -0600

    add badges code non-functional

--- db/badges.py
+++ db/badges.py
@@ -1,1 +1,15 @@
+import tindydb
+import badges.txt
+#fix cause it doesnt work
+def load_db_badges():
+    for each in badges.txt:
+        badges = badges.txt[each]
+        db.insert(badges)
+    return tinydb.Tinydb("badges.json")
+
+def appendbadges(db, name, value, description):
+    db.insert({"name":name, "value":value, "description":description})
+
+def lockedbadges(db, name, value, description, requirements):
+
 
