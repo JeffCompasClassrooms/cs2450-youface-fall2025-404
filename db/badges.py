c5c520e105fb94dde2a42b7059d4770ade7d3481
Ally Yeung <ayeung615@gmail.com>
2025-10-08 14:25:43 -0600

    did some css

--- db/badges.py
+++ db/badges.py
@@ -1,14 +1,17 @@
-import tindydb
-import badges.txt
+import tinydb
+# import badges.txt
 #fix cause it doesnt work
-def load_db_badges():
-    for each in badges.txt:
-        badges = badges.txt[each]
-        db.insert(badges)
-    return tinydb.Tinydb("badges.json")
-
 def appendbadges(db, name, value, description):
     db.insert({"name":name, "value":value, "description":description})
+def load_db_badges():
+    # for each in badges.txt:
+    #     badges = badges.txt[each]
+    #     db.insert(badges)
+   db = tinydb.Tinydb("badges.json")
+   appendbadges(db, "100pts", 100, "Earned your first 100 points!")
+   return db
+
+
 
 def lockedbadges(db, name, value, description, requirements):
 
