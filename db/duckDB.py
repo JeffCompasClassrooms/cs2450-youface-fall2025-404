7a2dea30d64c201672893de39168a4d207b6ecb0
Ally Yeung <ayeung615@gmail.com>
2025-10-26 13:35:39 -0600

    fixed enter code bug

--- db/duckDB.py
+++ db/duckDB.py
@@ -1,6 +1,5 @@
 import tinydb
 from . import users, helpers
-from tinydb.operations import increment
 def add_duck(db, name, value, code):
     db.insert({"name":name, "value":value, "code": code})
 
@@ -17,15 +16,17 @@ def found_duck(username, code):
     db = load_ducks()
     Duck = tinydb.Query()
     result = db.search(Duck.code == int(code))
-    
+
     if not result:
-        return "Invalid code.",duck['name'], users.get(User.username == username)
+        return "Invalid code.",'none', users.get(User.username == username)
     duck = result[0]
     if duck['name'] not in user['ducks']:
         user['ducks'].append(duck['name'])
         user['points'] += duck['value']
         User = tinydb.Query()
         users.update({'ducks': user['ducks'], 'points': user['points']}, User.username == username)
+        # bdb = badges.load_db_badges()
+
         return '{} found!'.format(duck['name']), duck['name'], users.get(User.username == username)
     return "You have {} already.".format(duck['name']), duck['name'], users.get(User.username == username)
          #when duck found make sure to run addbadgetouser() from badges.py to add a badge to the user
